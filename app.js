4d07c90f4441e0b93a2b44376bdabfaf361b5fe6
Beemhuse <brightawah94@gmail.com>
2022-04-06 14:04:19 +0100

    removes console.log

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fetchAdvice = async()=>{
     const res = await fetch('https://api.adviceslip.com/advice')
     const data= await res.json()
-    console.log(data)
+    // console.log(data)
 
     document.getElementById('title').innerHTML=`ADVICE #${data.slip.id}`
     document.getElementById('text').innerHTML=`"${data.slip.advice}"`
